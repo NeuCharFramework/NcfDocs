6a10c9aa160f0d49eae8ca343e87bcffec9f6279
JeffreySu <zsu@senparc.com>
2024-12-19 23:31:10 +0800

    add search feature

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { createRequire } from 'node:module'
 import process from 'node:process'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
-// import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { shikiPlugin } from '@vuepress/plugin-shiki'
@@ -49,7 +49,7 @@ export default defineUserConfig({
 
   // configure default theme
   theme: defaultTheme({
-    hostname: 'https://www.ncf.pub',
+    hostname: 'https://doc.ncf.pub',
     logo: '/images/logo.png',
     repo: 'NeuCharFramework/NCF',
     docsRepo: 'NeuCharFramework/NcfDocs',
@@ -136,57 +136,57 @@ export default defineUserConfig({
 
   // use plugins
   plugins: [
-    // docsearchPlugin({
-    //  appId: '34YFD9IUQ2',
-    //  apiKey: '9a9058b8655746634e01071411c366b8',
-    //  indexName: 'vuepress',
-    //  searchParameters: {
-    //    facetFilters: ['tags:v2'],
-    //  },
-    //  locales: {
-    //    '/zh/': {
-    //      placeholder: '搜索文档',
-    //      translations: {
-    //        button: {
-    //          buttonText: '搜索文档',
-    //          buttonAriaLabel: '搜索文档',
-    //        },
-    //        modal: {
-    //          searchBox: {
-    //            resetButtonTitle: '清除查询条件',
-    //            resetButtonAriaLabel: '清除查询条件',
-    //            cancelButtonText: '取消',
-    //            cancelButtonAriaLabel: '取消',
-    //          },
-    //          startScreen: {
-    //            recentSearchesTitle: '搜索历史',
-    //            noRecentSearchesText: '没有搜索历史',
-    //            saveRecentSearchButtonTitle: '保存至搜索历史',
-    //            removeRecentSearchButtonTitle: '从搜索历史中移除',
-    //            favoriteSearchesTitle: '收藏',
-    //            removeFavoriteSearchButtonTitle: '从收藏中移除',
-    //          },
-    //          errorScreen: {
-    //            titleText: '无法获取结果',
-    //            helpText: '你可能需要检查你的网络连接',
-    //          },
-    //          footer: {
-    //            selectText: '选择',
-    //            navigateText: '切换',
-    //            closeText: '关闭',
-    //            searchByText: '搜索提供者',
-    //          },
-    //          noResultsScreen: {
-    //            noResultsText: '无法找到相关结果',
-    //            suggestedQueryText: '你可以尝试查询',
-    //            reportMissingResultsText: '你认为该查询应该有结果？',
-    //            reportMissingResultsLinkText: '点击反馈',
-    //          },
-    //        },
-    //      },
-    //    },
-    //  },
-    // }),
+    docsearchPlugin({
+      appId: '1L3DHPKENV',
+      apiKey: '27af3cfe62f074fbaad251d6b33c0412',
+      indexName: 'NeuCharFramework Docs',
+      searchParameters: {
+        facetFilters: [],
+      },
+      locales: {
+        '/zh/': {
+          placeholder: '搜索文档',
+          translations: {
+            button: {
+              buttonText: '搜索文档',
+              buttonAriaLabel: '搜索文档',
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: '清除查询条件',
+                resetButtonAriaLabel: '清除查询条件',
+                cancelButtonText: '取消',
+                cancelButtonAriaLabel: '取消',
+              },
+              startScreen: {
+                recentSearchesTitle: '搜索历史',
+                noRecentSearchesText: '没有搜索历史',
+                saveRecentSearchButtonTitle: '保存至搜索历史',
+                removeRecentSearchButtonTitle: '从搜索历史中移除',
+                favoriteSearchesTitle: '收藏',
+                removeFavoriteSearchButtonTitle: '从收藏中移除',
+              },
+              errorScreen: {
+                titleText: '无法获取结果',
+                helpText: '你可能需要检查你的网络连接',
+              },
+              footer: {
+                selectText: '选择',
+                navigateText: '切换',
+                closeText: '关闭',
+                searchByText: '搜索提供者',
+              },
+              noResultsScreen: {
+                noResultsText: '无法找到相关结果',
+                suggestedQueryText: '你可以尝试查询',
+                reportMissingResultsText: '你认为该查询应该有结果？',
+                reportMissingResultsLinkText: '点击反馈',
+              },
+            },
+          },
+        },
+      },
+    }),
     googleAnalyticsPlugin({
       // we have multiple deployments, which would use different id
       id: process.env.DOCS_GA_ID ?? '',
